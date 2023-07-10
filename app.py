27dad00891f44a1bdff91b09d1786cde250019ce
Yedidya Harris <80841344+deedeeharris@users.noreply.github.com>
2023-07-10 15:03:50 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,8 +2,29 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 import io
+import requests
+
+def download_file_from_drive(file_id, destination_path):
+    url = f"https://drive.google.com/uc?export=download&id={file_id}"
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        with open(destination_path, "wb") as file:
+            file.write(response.content)
+        print("File downloaded successfully.")
+    else:
+        print("Failed to download file.")
+
+# download json
+file_id = st.secrets["main_df"]
+destination_path = "df_with_grades.xlsx"
+download_file_from_drive(file_id, destination_path)
 
 
+# download json
+file_id = st.secrets["grades_df"]
+destination_path = "grades.xlsx"
+download_file_from_drive(file_id, destination_path)
 
 # Function to load the main DataFrame
 def load_data():
