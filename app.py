da35a46240531915b571f62814c9eeea791f2426
Yedidya Harris <80841344+deedeeharris@users.noreply.github.com>
2023-07-10 15:08:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -155,7 +155,7 @@ def main():
 
     # Page title and subtitle
     st.title('Course Details and Grades')
-    st.markdown('## Yedidya Harris, Bnaya Hami @ 71253, HUJI')
+    st.markdown('### Yedidya Harris, Bnaya Hami @ 71253, HUJI')
     st.subheader('Search by Course Number')
 
     # Course number search input
